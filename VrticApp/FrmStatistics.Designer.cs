d99df0121630b9c8405f5b2c78b24a5038812fce
lvukovic23 <vukovicluka0101@gmail.com>
2025-05-09 23:33:41 +0200

    Forma statistika dolazaka djece poboljsana

--- VrticApp/FrmStatistics.Designer.cs
+++ VrticApp/FrmStatistics.Designer.cs
@@ -28,12 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.cbGroup = new System.Windows.Forms.ComboBox();
+            this.lblGroup = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.dgvDetails = new System.Windows.Forms.DataGridView();
+            this.Datum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dolazak = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Odlazak = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UkupnoSati = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetails)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaption.Location = new System.Drawing.Point(140, 20);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(251, 25);
+            this.lblCaption.TabIndex = 11;
+            this.lblCaption.Text = "Statistika dolazaka djece";
+            // 
+            // cbGroup
+            // 
+            this.cbGroup.FormattingEnabled = true;
+            this.cbGroup.Location = new System.Drawing.Point(85, 65);
+            this.cbGroup.Name = "cbGroup";
+            this.cbGroup.Size = new System.Drawing.Size(175, 21);
+            this.cbGroup.TabIndex = 17;
+            // 
+            // lblGroup
+            // 
+            this.lblGroup.AutoSize = true;
+            this.lblGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGroup.Location = new System.Drawing.Point(18, 68);
+            this.lblGroup.Name = "lblGroup";
+            this.lblGroup.Size = new System.Drawing.Size(47, 16);
+            this.lblGroup.TabIndex = 16;
+            this.lblGroup.Text = "Grupa:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(175, 21);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInterval.Location = new System.Drawing.Point(18, 112);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(53, 16);
+            this.lblInterval.TabIndex = 18;
+            this.lblInterval.Text = "Interval:";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFilter.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnFilter.Location = new System.Drawing.Point(297, 102);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(204, 30);
+            this.btnFilter.TabIndex = 21;
+            this.btnFilter.Text = "Primijeni filter";
+            this.btnFilter.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Podaci:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.Location = new System.Drawing.Point(18, 156);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(28, 16);
+            this.lblFrom.TabIndex = 23;
+            this.lblFrom.Text = "Od:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.Location = new System.Drawing.Point(271, 160);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(28, 16);
+            this.lblTo.TabIndex = 24;
+            this.lblTo.Text = "Do:";
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.Location = new System.Drawing.Point(64, 156);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(157, 20);
+            this.dtFrom.TabIndex = 25;
+            // 
+            // dtTo
+            // 
+            this.dtTo.Location = new System.Drawing.Point(321, 156);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(162, 20);
+            this.dtTo.TabIndex = 26;
+            // 
+            // dgvDetails
+            // 
+            this.dgvDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetails.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Datum,
+            this.Dolazak,
+            this.Odlazak,
+            this.UkupnoSati});
+            this.dgvDetails.Location = new System.Drawing.Point(64, 440);
+            this.dgvDetails.Name = "dgvDetails";
+            this.dgvDetails.Size = new System.Drawing.Size(443, 122);
+            this.dgvDetails.TabIndex = 27;
+            // 
+            // Datum
+            // 
+            this.Datum.HeaderText = "Datum";
+            this.Datum.Name = "Datum";
+            // 
+            // Dolazak
+            // 
+            this.Dolazak.HeaderText = "Dolazak";
+            this.Dolazak.Name = "Dolazak";
+            // 
+            // Odlazak
+            // 
+            this.Odlazak.HeaderText = "Odlazak";
+            this.Odlazak.Name = "Odlazak";
+            // 
+            // UkupnoSati
+            // 
+            this.UkupnoSati.HeaderText = "UkupnoSati";
+            this.UkupnoSati.Name = "UkupnoSati";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(21, 191);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(537, 194);
+            this.chart1.TabIndex = 28;
+            this.chart1.Text = "chart1";
+            // 
+            // FrmStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(595, 603);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dgvDetails);
+            this.Controls.Add(this.dtTo);
+            this.Controls.Add(this.dtFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.cbGroup);
+            this.Controls.Add(this.lblGroup);
+            this.Controls.Add(this.lblCaption);
+            this.Name = "FrmStatistics";
             this.Text = "FrmStatistics";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetails)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCaption;
+        private System.Windows.Forms.ComboBox cbGroup;
+        private System.Windows.Forms.Label lblGroup;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.DataGridView dgvDetails;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Datum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dolazak;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Odlazak;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UkupnoSati;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
