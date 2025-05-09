951da1e60734c5809b042194b7db0a77ad1647e1
akuqi23 <kuqiardijan@gmail.com>
2025-05-09 23:27:11 +0200

    Dodane 3 forme - registracija, evidencija o djeci, i upravljanje gruoa. (minmalna funkcionalnost)

---
+++ VrticApp/Registracija.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace probaa
+{
+    public partial class Registracija : Form
+    {
+        public Registracija()
+        {
+            InitializeComponent();
+
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd-MM-yyyy";
+        }
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd-MM-yyyy"; 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close ();
+        }
+
+        private void Registracija_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
